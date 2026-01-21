4b8e37a9072c775c16da4e845f510a5c0cfc261a
Takasg12 <rabbaniraya465@gmail.com>
2026-01-21 21:38:29 +0700

    Add Push In Line 31

--- objects/build a recipe tracker/script.js
+++ objects/build a recipe tracker/script.js
@@ -28,6 +28,34 @@ const recipe1Name = recipe1.name;
 const recipe2CookingTime = recipe2[`cookingTime`];
 const recipe3Ingredients = recipe3[`ingredients`];
 
-console.log(recipe1Name);
-console.log(recipe2CookingTime);
-console.log(recipe3Ingredients);
+//menambahkan objek recipe ke dalam array recipes
+recipes.push(recipe1, recipe2, recipe3);
+const getTotalIngredients = (ingredients) => {
+    return ingredients.length;
+}
+
+//membuat level kesulitan berdasarkan waktu memasak
+const getDifficultyLevel = (cookingTime) => {
+    if (cookingTime <= 30) {
+        return "easy";
+    } else if (cookingTime <= 60) {
+        return "medium";
+    } else {
+        return "hard";
+    }
+}
+
+const recipe1TotalIngredients = getTotalIngredients(recipe1.ingredients);
+const recipe1DifficultyLevel = getDifficultyLevel(recipe1.cookingTime);
+console.log(recipe1TotalIngredients);
+console.log(recipe1DifficultyLevel);
+
+//memberikan nilai pada bagian totalIngredients dan difficultyLevel pada objek recipe1
+recipe1.totalIngredients = getTotalIngredients(recipe1.ingredients);
+recipe1.difficultyLevel = getDifficultyLevel(recipe1.cookingTime);
+recipe2.totalIngredients = getTotalIngredients(recipe2.ingredients);
+recipe2.difficultyLevel = getDifficultyLevel(recipe2.cookingTime);
+recipe3.totalIngredients = getTotalIngredients(recipe3.ingredients)
+recipe3.difficultyLevel = getDifficultyLevel(recipe3.cookingTime);
+
+console.log(recipes)
